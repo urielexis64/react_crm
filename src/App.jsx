522196b68e0f7c1780e221b3b7a4ff2033ed8614
Uriel Aispuro <urielalexis64@gmail.com>
2022-01-24 11:13:51 -0700

    add new and edit subroutes

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,17 @@
-import {useState} from "react";
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 import Layout from "./layout/Layout";
-import Login from "./layout/Login";
 import Home from "./pages/Home";
-import LoginForm from "./pages/LoginForm";
+import NewClient from "./pages/NewClient";
+import EditClient from "./pages/EditClient";
 
 function App() {
 	return (
 		<BrowserRouter>
 			<Routes>
-				<Route path='/' element={<Login />}>
-					<Route index element={<LoginForm />} />
-					<Route />
-				</Route>
 				<Route path='/clients' element={<Layout />}>
 					<Route index element={<Home />} />
+					<Route path='new' element={<NewClient />} />
+					<Route path='edit/:id' element={<EditClient />} />
 				</Route>
 			</Routes>
 		</BrowserRouter>
