b6fe0425f836e778e62bd402a3bf7d4d3c363a01
Uriel Aispuro <urielalexis64@gmail.com>
2022-01-24 10:24:24 -0700

    implemet react router v6

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
 import {useState} from "react";
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import Layout from "./layout/Layout";
+import Login from "./layout/Login";
+import Home from "./pages/Home";
+import LoginForm from "./pages/LoginForm";
 
 function App() {
-	return <h1>asd</h1>;
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path='/' element={<Login />}>
+					<Route index element={<LoginForm />} />
+					<Route />
+				</Route>
+				<Route path='/clients' element={<Layout />}>
+					<Route index element={<Home />} />
+				</Route>
+			</Routes>
+		</BrowserRouter>
+	);
 }
 
 export default App;
---
+++ src/pages/EditClient.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const EditClient = () => {
+	return (
+		<div>
+			<h1>edit client</h1>
+		</div>
+	);
+};
+
+export default EditClient;
